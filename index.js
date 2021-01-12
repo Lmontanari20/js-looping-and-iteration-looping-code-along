5706f8892d7b3d7bfcf3863e574075f917343424
Lucas Montanari <Lmonty2011@gmail.com>
2021-01-11 18:44:13 -0800

    commit tree 3b59df9c5f146e49b340be7dfe505a7dd0b1aa3e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names){
+    let result = []
+    for (let i = 0; i < names.length; i++) {
+        result.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return result
+};
+
+function countDown(num) {
+    for(let i = num; i >= 0; i--){
+        console.log(i);
+    }
+};
